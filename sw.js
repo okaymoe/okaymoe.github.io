66de46514ceecb33f4370c0d7b7a4294b3c16cf9
Moe <moe62197@gmail.com>
2022-08-19 17:43:54 -0400

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-32faef84819d6c927ce6.js"
+    "url": "webpack-runtime-ae1c51433155db53b197.js"
   },
   {
     "url": "framework-0f8de3526885dd073c5e.js"
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "3f39301cb9e64d60162dc7a62f9bbdc8"
+    "revision": "5205979d6a82a8e7ce852c514b45c2a1"
   },
   {
     "url": "polyfill-9c9ebe1b02b37ee9b470.js"
